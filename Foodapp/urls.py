0431c7eebeaca95c8651077bfd0e221640290932
Nadeem <shaiknadeem983@gmail.com>
2022-01-31 15:03:08 +0530

    updated urls.py

--- Foodapp/urls.py
+++ Foodapp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
-from Foodapp.views import Item,Category
+from Foodapp.views import Item,Category,Bill, Customer, OrderHistory
 from django.conf.urls.static import static
 from django.conf import  settings
 from rest_framework import routers                             
@@ -8,6 +8,9 @@ from rest_framework import routers
 router = routers.DefaultRouter()                                    
 router.register(r'items', Item) 
 router.register(r'category', Category) 
+router.register(r'orderhistory', OrderHistory)
+router.register(r'bill', Bill)
+router.register(r'customer', Customer)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
