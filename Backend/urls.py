5420f77a90d4b5d557ffa8b91b602344b0bc02ee
Nadeem <shaiknadeem983@gmail.com>
2022-02-01 12:16:25 +0530

    included singupAndLogin.urls

--- Backend/urls.py
+++ Backend/urls.py
@@ -18,5 +18,6 @@ from django.urls import path ,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("Foodapp.urls")),  
+    path("", include("Foodapp.urls")),
+    path('',include('singupAndLogin.urls')),  
 ]
