a3f95eb4a1120bf907c9b8f43f62d71be29541df
Nadeem <shaiknadeem983@gmail.com>
2022-02-02 16:35:32 +0530

    added Dropdown

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import Menu from './Components/Menu';
 import Categories from './Components/Categories';
-import items from './data';
+import items from "./Components/data";
+import { Dropdown } from "react-bootstrap";
+import { HiOutlineViewList } from "react-icons/hi";
 
-const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) => {
-    if (category === 'all') {
+    if (category === "all") {
       setMenuItems(items);
       return;
     }
@@ -19,16 +21,32 @@ function App() {
   };
 
   return (
-    <main>
-      <section className="menu section">
-        <div className="title">
-          <h2>our menu</h2>
-          <div className="underline"></div>
-        </div>
-        <Categories categories={categories} filterItems={filterItems} />
-        <Menu items={menuItems} />
-      </section>
-    </main>
+    <div>
+      <Dropdown>
+        <Dropdown.Toggle
+          variant="secondary"
+          id="dropdown-basic"
+          style={{ textAlign: "right" }}
+        >
+          <HiOutlineViewList />
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Orders History</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Logout</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <main>
+        <section className="menu section">
+          <div className="title">
+            <h2>our menu</h2>
+            <div className="underline"></div>
+          </div>
+          <Categories categories={categories} filterItems={filterItems} />
+          <Menu items={menuItems} />
+        </section>
+      </main>
+    </div>
   );
 }
 
