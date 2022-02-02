b89f9abf5c1bc04420f459757da6271b09503ed5
Tejaswini <tejaswininukala9@gmail.com>
2022-02-02 17:33:30 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,7 @@ import Menu from './Components/Menu';
 import Categories from './Components/Categories';
 import items from './Components/data';
 import { Dropdown } from "react-bootstrap";
-<<<<<<< HEAD
 import { HiOutlineViewList } from "react-icons/hi";
-
-=======
-// import { HiOutlineViewList } from "react-icons/hi";
->>>>>>> 086762efde7bb39fb95b9710538edf04f9070748
 
 const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
@@ -33,7 +28,7 @@ function App() {
           id="dropdown-basic"
           style={{ textAlign: "right" }}
         >
-          {/* <HiOutlineViewList /> */}
+           <HiOutlineViewList /> 
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
